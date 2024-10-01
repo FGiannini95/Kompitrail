bab04300fc0fea9e951d65db0ba930b44e08190d
federico <fgiannini95@gmail.com>
2024-10-01 18:46:48 +0200

    Reverting some stuff

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('hola Julia') ;
+  res.send('hola Fede') ;
 });
 
 module.exports = router;
