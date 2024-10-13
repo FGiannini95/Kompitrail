818da1ac850111d841af8a1222f3bab1597a43de
federico <fgiannini95@gmail.com>
2024-10-13 16:23:22 +0200

    Adding templete as an example in the controller

--- server/controllers/usersControllers.js
+++ server/controllers/usersControllers.js
@@ -2,6 +2,23 @@ const connection = require("../config/db");
 const bcrypt = require("bcrypt");
 //const jwt = require("jsonwebtoken");
 
+{
+  /*
+    template example
+    action = (req, res) => {
+    #destructuring
+    const {here what you need} = req.body
+    #sql connection
+    let sql = `SELECT * FROM user WHERE email = '${email}'`;
+    connection.query(sql, (error, result) => {
+      error
+        ? res.status(500).json({ error });
+        : res.status(200).json(result);
+    });
+    }
+  */
+}
+
 class usersControllers {
   createUser = (req, res) => {
     const { name, lastname, email, password } = req.body;
