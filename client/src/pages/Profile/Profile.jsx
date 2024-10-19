922ef8bda2cbfe7d895703a33692906c0d153a1e
federico <fgiannini95@gmail.com>
2024-10-19 19:00:04 +0200

    updating the worfkow

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
 export const Profile = () => {
-  return <h1>Profile</h1>;
+  return (
+    <div style={{ paddingTop: "50px" }}>
+      <h1>Perfil</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident nemo
+        mollitia culpa ea sunt nisi cumque illum! Praesentium dolor modi ipsam
+        voluptate cumque repellat quos?
+      </p>
+      <button>Hola</button>
+    </div>
+  );
 };
