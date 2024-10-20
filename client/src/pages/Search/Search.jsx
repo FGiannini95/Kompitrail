6390d29dbe6a798b6bd9399be2a4c78a8d32c2fd
federico <fgiannini95@gmail.com>
2024-10-20 13:20:24 +0200

    Adding a little animation in the topbar

--- client/src/pages/Search/Search.jsx
+++ client/src/pages/Search/Search.jsx
@@ -5,15 +5,58 @@ export const Search = () => {
     <div>
       <h2>Search</h2>
       <div>
-        <p>
+        <p style={{ textAlign: "justify" }}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque
           repudiandae architecto minus pariatur laborum distinctio nostrum
           deserunt! Aperiam, voluptates? Esse officiis cumque iste et deserunt.
         </p>
-        <p>
+        <p style={{ textAlign: "justify" }}>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error, iure!
         </p>
         <button>Go to Landing</button>
+        <p style={{ textAlign: "justify" }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, rem.
+          Sequi laudantium eligendi incidunt provident ea nisi ad perferendis
+          aliquam porro! Adipisci consequatur exercitationem debitis vero nihil
+          quod culpa? Ab earum repudiandae ducimus maxime voluptate. Error quos
+          laborum qui amet.
+        </p>
+        <p style={{ textAlign: "justify" }}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum libero
+          nemo eos accusamus facere fugiat iure ipsa perspiciatis corporis,
+          consequatur, doloremque omnis sed. Facilis id nihil quas, quisquam
+          nostrum beatae dolor rem voluptatem illo delectus eius, nemo
+          perspiciatis, vel necessitatibus unde quis similique! Totam veritatis
+          maxime, maiores harum et nobis quibusdam aperiam consequatur vero
+          tempore.
+        </p>
+        <p style={{ textAlign: "justify" }}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab
+          exercitationem amet animi corrupti velit cupiditate, deserunt
+          asperiores alias fugit eum voluptates quos laborum eos, ducimus
+          obcaecati voluptatum nihil ipsum minus quibusdam recusandae quasi!
+          Autem beatae itaque ipsum, reprehenderit neque officia nihil sed nobis
+          pariatur ut architecto. Autem ratione labore possimus ex cumque
+          aperiam nisi facilis ut dolorem, corporis tempore inventore, molestias
+          placeat fugiat molestiae necessitatibus quam veniam earum dolores cum
+          quas! Aspernatur, illum ratione sed veniam commodi enim magni. Maxime
+          ab delectus sequi similique nemo magni totam inventore provident
+          accusantium!
+        </p>
+        <p style={{ textAlign: "justify" }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum vel
+          architecto in incidunt assumenda culpa expedita ipsam suscipit,
+          doloremque fuga minus nisi sunt atque neque voluptate exercitationem
+          molestias velit a laudantium commodi veritatis laboriosam saepe totam.
+          Sapiente quis, officia dolore explicabo officiis illum, corporis totam
+          asperiores eligendi non tempora architecto nesciunt corrupti
+          blanditiis eveniet quibusdam, error animi. Minus quis ducimus eius qui
+          labore dolore, architecto molestias a incidunt excepturi est
+          voluptatem laborum esse tenetur placeat possimus officiis saepe velit
+          sunt sit pariatur iure deserunt! Ut molestias exercitationem,
+          accusantium eaque quos explicabo voluptatibus, numquam suscipit magni
+          eius alias, nihil voluptate cupiditate.
+        </p>
       </div>
     </div>
   );
