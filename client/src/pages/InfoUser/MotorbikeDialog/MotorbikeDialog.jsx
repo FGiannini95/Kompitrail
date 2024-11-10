af206e1c6f921c59efa6f729888d7d7421c4e814
federico <fgiannini95@gmail.com>
2024-11-10 18:39:36 +0100

    Add pic in the dialog

--- client/src/pages/InfoUser/MotorbikeDialog/MotorbikeDialog.jsx
+++ client/src/pages/InfoUser/MotorbikeDialog/MotorbikeDialog.jsx
@@ -1,25 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
-import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import FileUploadOutlinedIcon from "@mui/icons-material/FileUploadOutlined";
 
 export const MotorbikeDialog = ({ openDialog, handleCloseDialog }) => {
+  const [brand, setBrand] = useState("");
+  const [model, setModel] = useState("");
+  const [photo, setPhoto] = useState();
+
+  const handleConfirm = () => {
+    console.log("Moto añadida:", { brand, model });
+    handleCloseDialog();
+  };
+
+  const handlePhotoChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setPhoto(file); // Guardar el archivo en el estado
+    }
+  };
+
   return (
     <Dialog open={openDialog} onClose={handleCloseDialog}>
       <DialogTitle>Añadir moto</DialogTitle>
       <DialogContent>
-        <Typography>
-          Aquí va el formulario para agregar o modificar una moto.
-        </Typography>
+        <Button
+          variant="text"
+          component="label"
+          fullWidth
+          sx={{ marginTop: 2 }}
+        >
+          <FileUploadOutlinedIcon />
+          {photo ? `Foto: ${photo.name}` : "Foto"}
+          <input
+            type="file"
+            accept="image/*"
+            hidden
+            onChange={handlePhotoChange}
+          />
+        </Button>
+        <TextField
+          label="Marca de la moto"
+          fullWidth
+          margin="normal"
+          value={brand}
+          onChange={(e) => setBrand(e.target.value)}
+        />
+        <TextField
+          label="Modelo de la moto"
+          fullWidth
+          margin="normal"
+          value={model}
+          onChange={(e) => setModel(e.target.value)}
+        />
       </DialogContent>
       <DialogActions>
         <Button onClick={handleCloseDialog} color="primary">
           Cancelar
         </Button>
-        <Button color="secondary">Confirmar</Button>
+        <Button onClick={handleConfirm} color="secondary">
+          Confirmar
+        </Button>
       </DialogActions>
     </Dialog>
   );
