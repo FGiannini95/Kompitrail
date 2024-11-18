68d2ec262eaf3b236557c491ab9fd10e3750f3b1
federico <fgiannini95@gmail.com>
2024-11-18 16:17:46 +0100

    ChatBot

---
+++ client/src/pages/InfoUser/HelpAndSupport/ChatoBot/ChatBot.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const ChatBot = () => {
+  return (
+    <div>
+      <h1>estamos en chatbot</h1>
+      <p>a ver si consigo que funcione</p>
+    </div>
+  );
+};
