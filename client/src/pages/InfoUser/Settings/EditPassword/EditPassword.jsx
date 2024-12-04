7dfc18e7c6a2cafa35351aa16f5581e5c8f14f6e
federico <fgiannini95@gmail.com>
2024-12-04 20:34:34 +0100

    Adding input to the view

--- client/src/pages/InfoUser/Settings/EditPassword/EditPassword.jsx
+++ client/src/pages/InfoUser/Settings/EditPassword/EditPassword.jsx
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 //MUI
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
 
 //MUI-ICONS
-import IconButton from "@mui/material/IconButton";
 import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import IconButton from "@mui/material/IconButton";
+import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
 
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export const EditPassword = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [, setIsPasswordSelected] = useState(false);
   const navigate = useNavigate();
+
+  const displayPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleFocus = () => {
+    setIsPasswordSelected(true);
+  };
+
+  const handleBlur = () => {
+    setIsPasswordSelected(false);
+  };
+
   return (
     <Grid container direction="column" spacing={2}>
       <Grid item container alignItems="center" justifyContent="space-between">
@@ -23,6 +42,74 @@ export const EditPassword = () => {
         <Button variant="text" color="black" disabled>
           Guardar
         </Button>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          label="Contraseña actual"
+          name="password"
+          type={showPassword ? "text" : "password"}
+          fullWidth
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          InputProps={{
+            endAdornment: (
+              <Button onClick={displayPassword}>
+                {showPassword ? (
+                  <VisibilityOffOutlinedIcon sx={{ color: "black" }} />
+                ) : (
+                  <VisibilityOutlinedIcon sx={{ color: "black" }} />
+                )}
+              </Button>
+            ),
+          }}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Typography>
+          La nueva contraseña tiene que incluir x carácteres y un seño especial.
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          label="Nueva contraseña"
+          name="password"
+          type={showPassword ? "text" : "password"}
+          fullWidth
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          InputProps={{
+            endAdornment: (
+              <Button onClick={displayPassword}>
+                {showPassword ? (
+                  <VisibilityOffOutlinedIcon sx={{ color: "black" }} />
+                ) : (
+                  <VisibilityOutlinedIcon sx={{ color: "black" }} />
+                )}
+              </Button>
+            ),
+          }}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TextField
+          label="Confirmar contraseña"
+          name="password"
+          type={showPassword ? "text" : "password"}
+          fullWidth
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          InputProps={{
+            endAdornment: (
+              <Button onClick={displayPassword}>
+                {showPassword ? (
+                  <VisibilityOffOutlinedIcon sx={{ color: "black" }} />
+                ) : (
+                  <VisibilityOutlinedIcon sx={{ color: "black" }} />
+                )}
+              </Button>
+            ),
+          }}
+        />
       </Grid>
     </Grid>
   );
