cdc10954ee42a858d5a413a56b24bf463ee14ea4
federico <fgiannini95@gmail.com>
2025-01-13 20:58:34 +0100

    Preparing structure for the routes card

--- client/src/pages/InfoUser/Route/MyRoute.jsx
+++ client/src/pages/InfoUser/Route/MyRoute.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // MUI
 import Typography from "@mui/material/Typography";
@@ -10,8 +10,10 @@ import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
 
 import { useNavigate } from "react-router-dom";
 import { RoutesString } from "../../../routes/routes";
+import { RouteCard } from "./RouteCard/RouteCard";
 
 export const MyRoute = () => {
+  const [allRoutes, setAllRoutes] = useState([]);
   const navigate = useNavigate();
 
   return (
@@ -23,6 +25,26 @@ export const MyRoute = () => {
         <Typography variant="h6">Mis rutas</Typography>
       </Grid>
       {/* Map allRoute and display in a card, divide between active and old ones */}
+      <Grid item container direction="column" spacing={2}>
+        {allRoutes.map((route) => {
+          <Grid
+            key={route?.routes_id}
+            container
+            spacing={1}
+            sx={{
+              marginTop: "10px",
+              marginLeft: "45px",
+              width: "100%",
+              textAlign: "center",
+            }}
+          >
+            <Grid item xs={12}>
+              {/* We have to pass down all the props */}
+              <RouteCard />
+            </Grid>
+          </Grid>;
+        })}
+      </Grid>
     </Grid>
   );
 };
---
+++ client/src/pages/InfoUser/Route/RouteCard/RouteCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const RouteCard = () => {
+  return (
+    <div>
+      <h1>Hola</h1>
+      <h2>again</h2>
+    </div>
+  );
+};
