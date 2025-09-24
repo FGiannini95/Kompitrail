a9732740574af8f1f208b1f4fe35b6f4fe2fff6e
federico <fgiannini95@gmail.com>
2025-09-24 16:32:46 +0100

    Use <li> rather then typography

--- client/src/pages/CreateTrip/CreateTrip.jsx
+++ client/src/pages/CreateTrip/CreateTrip.jsx
@@ -44,21 +44,20 @@ export const CreateTrip = () => {
         }}
       >
         <Typography>Personaliza tu ruta: </Typography>
-        <Typography>
-          define tu punto de salida y llegada, selecciona el nivel de
-          dificultad, y añade una descripción única.
-        </Typography>
-        <Typography>
-          ¡Hazla más especial indicando el tipo de moto adecuado y el número de
-          participantes!
-        </Typography>
-        <Typography>
-          ¿Primera vez o ya conocida? Marca si tu ruta está verificada y
-          establece el tiempo estimado para completarla.
-        </Typography>
-        <Typography>
-          ¡Empieza a planificar tu próxima aventura ahora!
-        </Typography>
+        <ol>
+          <li>Define tu punto de salida y de llegada</li>
+          <li>Selecciona el nivel de dificultad</li>
+          <li>Añade una descripción única.</li>
+          <li>
+            ¡Hazla más especial indicando el tipo de moto adecuado y el número
+            de participantes!
+          </li>
+          <li>
+            ¿Primera vez o ya conocida? Marca si tu ruta está verificada y
+            establece el tiempo estimado para completarla.
+          </li>
+          <li>¡Empieza a planificar tu próxima aventura ahora!</li>
+        </ol>
       </Grid>
       <Grid container direction="column" spacing={2}>
         <Grid item>
