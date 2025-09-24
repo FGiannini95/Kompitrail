9d9b497655db7ee3f55091ce21836b614785d03a
federico <fgiannini95@gmail.com>
2025-09-24 09:43:48 +0100

    revert changes

--- client/src/helpers/Utils.js
+++
@@ -1,6 +0,0 @@
-export const getInitials = (name, lastname) => {
-  const firstLetterName = name?.charAt(0).toUpperCase() || "";
-  const firstLetterLastName = lastname?.charAt(0).toUpperCase() || "";
-
-  return `${firstLetterName}${firstLetterLastName}`;
-};
