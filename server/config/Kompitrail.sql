5b9ebde33c1a4e22d888e7966b1a48400821aa13
federico <fgiannini95@gmail.com>
2025-09-26 10:54:37 +0100

    New db

--- server/config/Kompitrail.sql
+++ server/config/Kompitrail.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
--- Host: localhost    Database: kompitrail
+-- Host: 127.0.0.1    Database: kompitrail
 -- ------------------------------------------------------
 -- Server version	8.0.34
 
@@ -14,6 +14,73 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `motorbike`
+--
+
+DROP TABLE IF EXISTS `motorbike`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `motorbike` (
+  `motorbike_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int unsigned NOT NULL,
+  `motorbike_brand` varchar(50) NOT NULL,
+  `motorbike_model` varchar(50) DEFAULT NULL,
+  `img` varchar(150) DEFAULT NULL,
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`motorbike_id`),
+  KEY `fk_user_id` (`user_id`),
+  CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `motorbike`
+--
+
+LOCK TABLES `motorbike` WRITE;
+/*!40000 ALTER TABLE `motorbike` DISABLE KEYS */;
+/*!40000 ALTER TABLE `motorbike` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `route`
+--
+
+DROP TABLE IF EXISTS `route`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `route` (
+  `route_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int unsigned NOT NULL,
+  `route_name` varchar(50) NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `starting_point` varchar(80) NOT NULL,
+  `ending_point` varchar(80) NOT NULL,
+  `level` varchar(50) NOT NULL,
+  `distance` smallint unsigned NOT NULL,
+  `is_verified` tinyint(1) NOT NULL DEFAULT '0',
+  `suitable_motorbike_type` varchar(70) NOT NULL,
+  `estimated_time` tinyint unsigned NOT NULL,
+  `participants` tinyint unsigned NOT NULL,
+  `route_description` varchar(250) DEFAULT NULL,
+  `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`route_id`),
+  KEY `fk_user_2` (`user_id`),
+  CONSTRAINT `fk_user_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `route`
+--
+
+LOCK TABLES `route` WRITE;
+/*!40000 ALTER TABLE `route` DISABLE KEYS */;
+/*!40000 ALTER TABLE `route` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `user`
@@ -35,7 +102,6 @@ CREATE TABLE `user` (
   `is_deleted` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `nickname` (`nickname`),
   UNIQUE KEY `phonenumber` (`phonenumber`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -58,4 +124,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-03 19:00:04
+-- Dump completed on 2025-09-26 10:50:42
