53cc825ef8bdea938537ed1e5a1849d65652f1b3
federico <fgiannini95@gmail.com>
2025-09-27 23:38:46 +0100

    UserAvatar + refactor

--- client/src/pages/InfoUser/InfoUser.jsx
+++ client/src/pages/InfoUser/InfoUser.jsx
@@ -11,6 +11,7 @@ import {
   DialogTitle,
   Tooltip,
   List,
+  Stack,
 } from "@mui/material";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
 import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
@@ -22,20 +23,37 @@ import { delLocalStorage } from "../../helpers/localStorageUtils";
 import { useNavigate } from "react-router-dom";
 import { RoutesString } from "../../routes/routes";
 import { KompitrailContext } from "../../context/KompitrailContext";
-import { capitalizeFullName, getInitials } from "../../helpers/utils";
 import { SettingsRow } from "./Settings/SettingsRow/SettingsRow";
 import { PrivacyDialog } from "./Privacy/PrivacyDialog";
+import { UserAvatar } from "../../components/UserAvatar/UserAvatar";
+
+function Section({ title, children }) {
+  return (
+    <Box
+      sx={{
+        mt: "30px",
+        p: "10px",
+        bgcolor: "#eeeeee",
+        mx: "10px",
+        borderRadius: "20px",
+      }}
+    >
+      <Typography variant="h6" sx={{ fontWeight: "bold", pb: 1 }}>
+        {title}
+      </Typography>
+      <List disablePadding>{children}</List>
+    </Box>
+  );
+}
 
 export const InfoUser = () => {
-  const { user, setUser, setToken, setIsLogged } =
-    useContext(KompitrailContext);
+  const { setUser, setToken, setIsLogged } = useContext(KompitrailContext);
   const [dialog, setDialog] = useState(false);
   const [iframe, setiIframe] = useState(false);
   const [iframeUrl, setIframeUrl] = useState("");
   const [isCopied, setIsCopied] = useState(false);
 
   const navigate = useNavigate();
-  const initials = getInitials(user.name, user.lastname);
 
   const logOut = () => {
     delLocalStorage("token");
@@ -52,10 +70,6 @@ export const InfoUser = () => {
   const handleConfirmation = () => {
     logOut();
     setDialog(false);
-  };
-
-  const handleCancel = () => {
-    navigate("/");
   };
 
   const handleToggleIframe = (url) => {
@@ -76,206 +90,103 @@ export const InfoUser = () => {
 
   return (
     <Box
-      style={{
+      sx={{
         backgroundColor: "#fafafa",
         paddingTop: "25px",
       }}
     >
+      {/* Back/close icon */}
       <Grid>
         <CloseOutlinedIcon
-          style={{ paddingLeft: "20px" }}
-          onClick={handleCancel}
+          sx={{ paddingLeft: "20px", cursor: "pointer" }}
+          onClick={() => navigate("/")}
         />
       </Grid>
-      <Grid>
-        <Grid
-          container
-          spacing={1}
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <Grid
-            xs={1}
-            container
-            spacing={0}
-            direction="column"
-            alignItems="center"
-            justifyContent="center"
-            sx={{
-              width: 1 / 3,
-              border: "2px solid black",
-              borderRadius: "50%",
-              aspectRatio: 1 / 1,
-            }}
-            style={{ paddingTop: "0px", paddingLeft: "0px" }}
-          >
-            <Typography sx={{}} variant="h4">
-              {initials}
-            </Typography>
-          </Grid>
-          <Grid xs={12}>
-            <Typography variant="h5" sx={{ fontWeight: "bold" }}>
-              {capitalizeFullName(user.name, user.lastname)}
-            </Typography>
-          </Grid>
-        </Grid>
-        <Grid
-          container
-          alignItems="center"
-          justifyContent="center"
-          spacing={2}
-          style={{
-            paddingTop: "25px",
-            paddingLeft: "20px",
-            marginBottom: "30px",
+      {/* User header */}
+      <UserAvatar />
+      {/* Buttons actions */}
+      <Stack
+        direction="row"
+        spacing={2}
+        justifyContent="center"
+        sx={{ p: "10px" }}
+      >
+        <Button
+          type="button"
+          variant="contained"
+          sx={{
+            color: "black",
+            boxShadow: "none",
+            backgroundColor: "#eeeeee",
+            "&:hover": { backgroundColor: "#dddddd" },
           }}
+          fullWidth
         >
-          <Grid style={{ paddingRight: "10px" }}>
-            <Button
-              type="button"
-              variant="contained"
-              sx={{
-                color: "black",
-                boxShadow: "none",
-                backgroundColor: "#eeeeee",
-                "&:hover": { backgroundColor: "#dddddd" },
-              }}
-              fullWidth
-            >
-              Ir a premium
-            </Button>
-          </Grid>
-          <Grid>
-            <Tooltip
-              title="URL copiada"
-              open={isCopied} // Display the tooltip only if isCopied is true
-              disableInteractive // It doesn't appear with the interaction of the mouse
-              arrow // Display the arrow
-            >
-              <Button
-                type="button"
-                variant="outlined"
-                color="secondary"
-                fullWidth
-                sx={{
-                  color: "black",
-                  borderColor: "#eeeeee",
-                  borderWidth: "2px",
-                  "&:hover": {
-                    borderColor: "#dddddd",
-                    borderWidth: "2px",
-                  },
-                }}
-                onClick={handleShare}
-              >
-                Compartir perfil
-                <ShareOutlinedIcon
-                  style={{ paddingLeft: "5px", width: "20px" }}
-                />
-              </Button>
-            </Tooltip>
-          </Grid>
-        </Grid>
-      </Grid>
+          Ir a premium
+        </Button>
+        <Tooltip
+          title="URL copiada"
+          open={isCopied} // Display the tooltip only if isCopied is true
+          disableInteractive // It doesn't appear with the interaction of the mouse
+          arrow // Display the arrow
+        >
+          <Button
+            type="button"
+            variant="outlined"
+            color="secondary"
+            fullWidth
+            sx={{
+              color: "black",
+              borderColor: "#eeeeee",
+              borderWidth: "2px",
+              "&:hover": {
+                borderColor: "#dddddd",
+                borderWidth: "2px",
+              },
+            }}
+            onClick={handleShare}
+          >
+            Compartir perfil
+            <ShareOutlinedIcon style={{ paddingLeft: "5px", width: "20px" }} />
+          </Button>
+        </Tooltip>
+      </Stack>
 
-      <Grid
-        style={{
-          marginTop: "30px",
-          padding: "10px",
-          paddingLeft: "20px",
-          backgroundColor: "#eeeeee",
-          margin: "10px",
-          borderRadius: "20px",
-        }}
-      >
-        <Typography
-          variant="h6"
-          sx={{ fontWeight: "bold" }}
-          style={{ paddingBottom: "10px" }}
-        >
-          Mi cuenta
-        </Typography>
-        <Grid>
-          <List disablePadding>
-            {/* Edit profil option */}
-            <SettingsRow
-              action="editAccount"
-              onClick={() => navigate(RoutesString.editUser)}
-            />
-            {/* Add motorbike option */}
-            <SettingsRow
-              action="addMotorbike"
-              onClick={() => navigate(RoutesString.motorbike)}
-            />
-            {/* Add route option */}
-            <SettingsRow
-              action="addRoute"
-              onClick={() => navigate(RoutesString.route)}
-            />
-            {/* Settings option */}
-            <SettingsRow
-              action="changeSettings"
-              onClick={() => navigate(RoutesString.settings)}
-            />
-          </List>
-        </Grid>
-      </Grid>
-      <Grid
-        style={{
-          marginTop: "30px",
-          padding: "10px",
-          paddingLeft: "20px",
-          backgroundColor: "#eeeeee",
-          margin: "10px",
-          borderRadius: "20px",
-        }}
-      >
-        <Typography
-          variant="h6"
-          sx={{ fontWeight: "bold" }}
-          style={{ paddingBottom: "10px" }}
-        >
-          Ayuda y soporte
-        </Typography>
-        <List>
-          {/* Chatbot option */}
-          <SettingsRow
-            action="chatbot"
-            onClick={() => navigate(RoutesString.settings)}
-          />
-          {/* Privacy option */}
-          <SettingsRow
-            action="privacy"
-            onClick={() => handleToggleIframe(url)}
-          />
-        </List>
-      </Grid>
-      <Grid
-        style={{
-          marginTop: "30px",
-          padding: "10px",
-          paddingLeft: "20px",
-          backgroundColor: "#eeeeee",
-          margin: "10px",
-          borderRadius: "20px",
-        }}
-      >
-        <Typography
-          variant="h6"
-          sx={{ fontWeight: "bold" }}
-          style={{ paddingBottom: "10px" }}
-        >
-          Desconectar perfil
-        </Typography>
-        <List>
-          {/* Logout */}
-          <SettingsRow
-            action="logout"
-            onClick={() => handleToggleDialog("logout")}
-          />
-        </List>
-      </Grid>
+      <Section title="Mi cuenta">
+        <SettingsRow
+          action="editAccount"
+          onClick={() => navigate(RoutesString.editUser)}
+        />
+        <SettingsRow
+          action="addMotorbike"
+          onClick={() => navigate(RoutesString.motorbike)}
+        />
+        <SettingsRow
+          action="addRoute"
+          onClick={() => navigate(RoutesString.route)}
+        />
+        <SettingsRow
+          action="changeSettings"
+          onClick={() => navigate(RoutesString.settings)}
+        />
+      </Section>
+
+      <Section title="Ayuda y Soporte">
+        <SettingsRow
+          action="chatbot"
+          onClick={() => navigate(RoutesString.settings)}
+        />
+        <SettingsRow action="privacy" onClick={() => handleToggleIframe(url)} />
+      </Section>
+
+      <Section title="Desconectar perfil">
+        <SettingsRow
+          action="logout"
+          onClick={() => handleToggleDialog("logout")}
+        />
+      </Section>
+
+      {/* Remove the Dialog once ConfirmDialogProvider in implemented*/}
       <Dialog open={dialog} onClose={handleToggleDialog}>
         <DialogTitle>Cerrar sesión</DialogTitle>
         <DialogContent>
@@ -292,7 +203,7 @@ export const InfoUser = () => {
       </Dialog>
       <PrivacyDialog
         openIframe={iframe}
-        handleCloseIframe={handleToggleIframe}
+        handleCloseIframe={() => setiIframe(false)}
         iframeUrl={iframeUrl}
       />
     </Box>
