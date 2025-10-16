53506de20eaddbcb3e66f831bc279ae7d4c08111
federico <fgiannini95@gmail.com>
2025-10-16 15:30:45 +0100

    Improve PlusAvatar Ui

--- client/src/components/PlusAvatar/PlusAvatar.jsx
+++ client/src/components/PlusAvatar/PlusAvatar.jsx
@@ -4,13 +4,19 @@ import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
 
 export const PlusAvatar = ({ size = 40, onClick }) => {
   return (
-    <Stack alignItems="center" spacing={1} flexDirection="column">
+    <Stack alignItems="center" spacing={0.5} sx={{ width: size + 8 }}>
       <IconButton
         onClick={(e) => {
           e.stopPropagation();
           onClick?.();
         }}
-        sx={{ p: 0 }}
+        sx={{
+          p: 0,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          gap: 0.5,
+        }}
       >
         <Avatar
           // src={user?.photoUrl || undefined}
