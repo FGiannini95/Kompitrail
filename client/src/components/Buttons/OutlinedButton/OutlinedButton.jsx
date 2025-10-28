62db2fe5db31f3e810c290eee89ec8647eaeed40
federico <fgiannini95@gmail.com>
2025-10-28 10:06:21 +0000

    Remove warning

--- client/src/components/Buttons/OutlinedButton/OutlinedButton.jsx
+++ client/src/components/Buttons/OutlinedButton/OutlinedButton.jsx
@@ -1,25 +1,32 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { Button } from "@mui/material";
 
-export const OutlinedButton = ({ onClick, text, icon }) => {
-  return (
-    <Button
-      type="button"
-      variant="outlined"
-      fullWidth
-      onClick={onClick}
-      sx={{
-        color: "black",
-        borderColor: "#eeeeee",
-        borderWidth: "2px",
-        "&:hover": {
-          borderColor: "#dddddd",
+export const OutlinedButton = forwardRef(
+  ({ onClick, text, icon, sx, ...rest }, ref) => {
+    return (
+      <Button
+        ref={ref}
+        type="button"
+        variant="outlined"
+        fullWidth
+        onClick={onClick}
+        sx={{
+          color: "black",
+          borderColor: "#eeeeee",
           borderWidth: "2px",
-        },
-      }}
-    >
-      {text}
-      {icon}
-    </Button>
-  );
-};
+          "&:hover": {
+            borderColor: "#dddddd",
+            borderWidth: "2px",
+          },
+          ...sx,
+        }}
+        {...rest}
+      >
+        {text}
+        {icon}
+      </Button>
+    );
+  }
+);
+
+OutlinedButton.displayName = "OutlinedButton";
