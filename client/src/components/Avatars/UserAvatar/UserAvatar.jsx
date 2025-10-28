3b03957aacf7b28085f15bedac3bc8dfaba63b1f
federico <fgiannini95@gmail.com>
2025-10-28 12:23:54 +0000

    Display pic full screen

--- client/src/components/Avatars/UserAvatar/UserAvatar.jsx
+++ client/src/components/Avatars/UserAvatar/UserAvatar.jsx
@@ -1,7 +1,8 @@
-import React, { useContext, useMemo } from "react";
+import React, { useContext, useMemo, useState } from "react";
 import { Stack, Typography, Avatar, Box } from "@mui/material";
 import { getInitials } from "../../../helpers/utils";
 import { KompitrailContext } from "../../../context/KompitrailContext";
+import { FullScreenImg } from "../../FullScreenImg/FullScreenImg";
 
 export const UserAvatar = () => {
   const { user } = useContext(KompitrailContext);
@@ -15,36 +16,46 @@ export const UserAvatar = () => {
     ? `http://localhost:3000/images/users/${user.img}`
     : null;
 
+  const [openImg, setOpenImg] = useState(false);
+  const handleOpenImg = () => setOpenImg(true);
+  const handleCloseImg = () => setOpenImg(false);
+
   return (
-    <Stack alignItems="center" spacing={1} sx={{ mx: "auto" }}>
-      <Avatar
-        src={photoUrl || undefined}
-        sx={{
-          width: 96,
-          height: 96,
-          fontSize: 32,
-          border: "2px solid black",
-          color: "black",
-          backgroundColor: "transparent",
-        }}
-      >
-        {!photoUrl && initials}
-      </Avatar>
-      <Box
-        sx={{ width: "100%", maxWidth: 360, mx: "auto", textAlign: "center" }}
-      >
-        <Typography
-          variant="h5"
+    <>
+      <Stack alignItems="center" spacing={1} sx={{ mx: "auto" }}>
+        <Avatar
+          src={photoUrl || undefined}
           sx={{
-            fontWeight: "bold",
-            whiteSpace: "normal",
-            wordBreak: "break-word",
-            overflowWrap: "break-word",
+            width: 96,
+            height: 96,
+            fontSize: 32,
+            border: "2px solid black",
+            color: "black",
+            backgroundColor: "transparent",
           }}
+          onClick={handleOpenImg}
         >
-          {fullName}
-        </Typography>
-      </Box>
-    </Stack>
+          {!photoUrl && initials}
+        </Avatar>
+        <Box
+          sx={{ width: "100%", maxWidth: 360, mx: "auto", textAlign: "center" }}
+        >
+          <Typography
+            variant="h5"
+            sx={{
+              fontWeight: "bold",
+              whiteSpace: "normal",
+              wordBreak: "break-word",
+              overflowWrap: "break-word",
+            }}
+          >
+            {fullName}
+          </Typography>
+        </Box>
+      </Stack>
+      {photoUrl && (
+        <FullScreenImg open={openImg} onClose={handleCloseImg} img={photoUrl} />
+      )}
+    </>
   );
 };
