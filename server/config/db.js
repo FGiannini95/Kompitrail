84235fe88738f0d40e265e2beaf2c20b76845a24
federico <fgiannini95@gmail.com>
2025-10-30 12:26:54 +0000

    Add ssl config

--- server/config/db.js
+++ server/config/db.js
@@ -1,20 +1,21 @@
-const mysql2 = require('mysql2');
-require('dotenv').config();
+const mysql2 = require("mysql2");
+require("dotenv").config();
 
 const connection = mysql2.createConnection({
-  host: process.env.DB_HOST, 
-  user: process.env.DB_USER, 
-  password: process.env.DB_PASS, 
-  database: process.env.DB_NAME
-})
-
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  //TiDB Serverless:
+  ssl: { minVersion: "TLSv1.2", rejectUnauthorized: true },
+});
 
 connection.connect((error) => {
-  if(error) {
+  if (error) {
     throw error;
   } else {
     console.log("Conexión a bd correcta");
   }
 });
 
-module.exports = connection; 
+module.exports = connection;
