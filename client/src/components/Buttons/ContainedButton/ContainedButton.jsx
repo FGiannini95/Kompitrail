6431a1320c331d622e40167c895bf7f9a4bc8370
federico <fgiannini95@gmail.com>
2025-11-06 10:42:46 +0000

    forwardRef in ContainedButton

--- client/src/components/Buttons/ContainedButton/ContainedButton.jsx
+++ client/src/components/Buttons/ContainedButton/ContainedButton.jsx
@@ -1,22 +1,28 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import { Button } from "@mui/material";
 
-export const ContainedButton = ({ onClick, text, icon }) => {
-  return (
-    <Button
-      type="button"
-      variant="contained"
-      fullWidth
-      onClick={onClick}
-      sx={{
-        color: "black",
-        boxShadow: "none",
-        backgroundColor: "#eeeeee",
-        "&:hover": { backgroundColor: "#dddddd" },
-      }}
-    >
-      {text}
-      {icon}
-    </Button>
-  );
-};
+export const ContainedButton = forwardRef(
+  ({ onClick, text, icon, ...rest }, ref) => {
+    return (
+      <Button
+        ref={ref}
+        type="button"
+        variant="contained"
+        fullWidth
+        onClick={onClick}
+        sx={{
+          color: "black",
+          boxShadow: "none",
+          backgroundColor: "#eeeeee",
+          "&:hover": { backgroundColor: "#dddddd" },
+        }}
+        {...rest}
+      >
+        {text}
+        {icon}
+      </Button>
+    );
+  }
+);
+
+ContainedButton.displayName = "ContainedButton";
