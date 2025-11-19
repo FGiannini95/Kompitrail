c263df90fd05affe504578c9f6f45e85158e445a
federico <fgiannini95@gmail.com>
2025-11-19 23:38:05 +0000

    Add chat controller

---
+++ server/controllers/chatControllers.js
@@ -0,0 +1,8 @@
+const connection = require("../config/db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+class chatControllers {}
+
+module.exports = new chatControllers();
---
+++ server/routes/chat.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var router = express.Router();
+const chatControllers = require("../controllers/chatControllers");
+
+// ruta base http://localhost:3000/chat
+
+module.exports = router;
