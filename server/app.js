c6eb156615b76c0932441c091438e84cc751e6b9
federico <fgiannini95@gmail.com>
2026-02-18 11:26:32 +0000

    Fix cors

--- server/app.js
+++ server/app.js
@@ -39,6 +39,7 @@ app.use(
   cors({
     origin: [
       process.env.FRONTEND_URL || "http://localhost:5173",
+      "http://127.0.0.1:5173",
       "http://192.168.0.43:5173",
     ],
     credentials: true,
